fdba4f140cc5d671e40b071f4813f712a9cd9832
Yana Ivanova <yana@plam.ch>
2022-08-11 17:46:52 +0300

    style: improve layout

--- src/App.tsx
+++ src/App.tsx
@@ -23,56 +23,54 @@ export function App() {
             <p>Nous allons reéssayer automatiquement à chaque {pollingIntervalMinutes} minutes.</p>
         </div>
     ) : (
-        <div className="container">
-            <div className="table-panel-container">
-                <div>
-                    <p>
-                        <strong className="welcome-text">
-                            {Intl.DateTimeFormat('fr-CH', { year: 'numeric', month: 'long', day: 'numeric' }).format(
-                                new Date()
-                            )}{' '}
-                        </strong>
-                        | Nous vous souhaitons la bienvenue dans les formations suivantes :
-                    </p>
-                    {courses?.length === 0 ? (
-                        <div>
-                            Aucune formation dans le prochain 1 heure et 30 minutes. Mise à jour automatique à chaque{' '}
-                            {pollingIntervalMinutes} minutes.
-                        </div>
-                    ) : (
-                        <TableContainer className="upcoming-courses-table" component={Paper}>
-                            <Table>
-                                <TableHead>
-                                    <TableRow>
-                                        <TableCell>Début</TableCell>
-                                        <TableCell>Formation</TableCell>
-                                        <TableCell>Etage</TableCell>
-                                        <TableCell>Salle</TableCell>
-                                        <TableCell>Formateur(s)</TableCell>
-                                    </TableRow>
-                                </TableHead>
-                                <TableBody>
-                                    {courses?.map((course) => (
-                                        <TableRow key={course.id}>
-                                            <TableCell>{`${formatDate({
-                                                dateString: course.start,
-                                                isTimeVisible: true,
-                                            })}`}</TableCell>
-                                            <TableCell>{course.name}</TableCell>
-                                            <TableCell className="no-wrap">{course.roomFloor}</TableCell>
-                                            <TableCell className="no-wrap">{course.roomName}</TableCell>
-                                            <TableCell>{course.teachers?.join(', ')}</TableCell>
-                                        </TableRow>
-                                    ))}
-                                </TableBody>
-                            </Table>
-                        </TableContainer>
+        <>
+            <p className="welcome-paragraph">
+                <strong className="welcome-text">
+                    {Intl.DateTimeFormat('fr-CH', { year: 'numeric', month: 'long', day: 'numeric' }).format(
+                        new Date()
                     )}
+                </strong>{' '}
+                | Nous vous souhaitons la bienvenue dans les formations suivantes :
+            </p>
+            {courses?.length === 0 ? (
+                <div>
+                    Aucune formation dans le prochain 1 heure et 30 minutes. Mise à jour automatique à chaque{' '}
+                    {pollingIntervalMinutes} minutes.
                 </div>
-            </div>
+            ) : (
+                <TableContainer className="upcoming-courses-table" component={Paper}>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Début</TableCell>
+                                <TableCell>Formation</TableCell>
+                                <TableCell>Etage</TableCell>
+                                <TableCell>Salle</TableCell>
+                                <TableCell>Formateur(s)</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {courses?.map((course) => (
+                                <TableRow key={course.id}>
+                                    <TableCell>{`${formatDate({
+                                        dateString: course.start,
+                                        isTimeVisible: true,
+                                    })}`}</TableCell>
+                                    <TableCell>{course.name}</TableCell>
+                                    <TableCell className="no-wrap">{course.roomFloor}</TableCell>
+                                    <TableCell className="no-wrap">{course.roomName}</TableCell>
+                                    <TableCell>{course.teachers?.join(', ')}</TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            )}
             <footer className="footer">
-                Retrouvez toutes nos formations sur <strong>www.cep.swiss</strong>
+                <p>
+                    Retrouvez toutes nos formations sur <strong>www.cep.swiss</strong>
+                </p>
             </footer>
-        </div>
+        </>
     )
 }
