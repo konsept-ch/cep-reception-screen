bfa426927d38ed7ef29aad42ba8039963efcabef
Yana Ivanova <yana@plam.ch>
2022-08-11 17:58:20 +0300

    style: customize font family

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
-import CssBaseline from '@mui/material/CssBaseline'
+import { ThemeProvider, CssBaseline, createTheme } from '@mui/material'
 
 import { store } from './app/store'
 import { App } from './App'
@@ -11,12 +11,20 @@ import './scss/index.scss'
 const container = document.getElementById('root')!
 const root = createRoot(container)
 
+const theme = createTheme({
+    typography: {
+        fontFamily: 'Overpass',
+    },
+})
+
 root.render(
     <StrictMode>
-        <CssBaseline />
-        <Provider store={store}>
-            <App />
-        </Provider>
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Provider store={store}>
+                <App />
+            </Provider>
+        </ThemeProvider>
     </StrictMode>
 )
 
